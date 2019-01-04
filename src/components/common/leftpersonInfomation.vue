8caa49afafe7854a73ad0fcba62629f840bd8f20
solaliuqi <1341220428@qq.com>
2019-01-04 20:34:09 +0800

    左上角样式修改2

--- src/components/common/leftpersonInfomation.vue
+++ src/components/common/leftpersonInfomation.vue
@@ -1,19 +1,18 @@
 <template>
   <el-card class="box-card">
-    <a class="u-bgUserColor u-block"
-       href="#"></a>
-    <div class="post">
+    <a class="u-bgUserColor u-block" href="#"></a>
+    <div class="lpost">
       <div class="person-header">
         <svg id="ficon"
              class="icon"
              aria-hidden="true">
           <use :xlink:href="'#favicon-default'+(person.iconId)"></use>
         </svg>
-      </div>
-      <div class="person-text">
         <div class="person-name">
           <a class="person-u">@{{person.userName}}</a>
         </div>
+      </div>
+      <div class="person-text">
         <div class="person-content">
           <div class="pleft">
             <span><strong>动态</strong></span>
@@ -30,7 +29,7 @@
 </template>
 <script>
 export default {
-
+  name: 'personInfomation.vue',
   data () {
     return {
       person:
@@ -45,50 +44,44 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style scoped lang='scss'>
 .box-card {
   width: 200px;
-  height: 200px;
-  /*margin-bottom: 10px;*/
+  height:180px;
   position: relative;
-}
-.u-bgUserColor {
-  background-color: #1da1f2 !important ;
-}
-.u-block {
-  display: block !important;
-  height: 70px;
-  width: 100%;
-  padding: 0;
-}
-.person-name {
-  position: absolute;
-  width: 185px;
-  left: 45px;
-  font-weight: bold;
-  font-size: 20px;
-}
-.person-content {
-  font-size: 14px;
-  padding-top: 70px;
-  padding-left: 0px;
-}
-.post {
-  width: 100%;
-}
-.pleft {
-  float: left;
-  padding-left: 10px;
-}
-.pright {
-  float: left;
-  padding-left: 35px;
-  /*padding-right: 10px;*/
-}
-#ficon {
-  width: 78px;
-  height: 78px;
-  margin: -40px 0 0 0;
-  float: left;
+  .u-bgUserColor {
+    background-color: #1da1f2 !important ;
+  }
+  .u-block {
+    display: block !important;
+    height: 70px;
+    width: 100%;
+    padding: 0;
+  }
+  .lpost {
+    width: 100%;
+    .person-name {
+      font-weight: bold;
+      font-size: 20px;
+    }
+    .person-content {
+      font-size: 14px;
+      padding-top: 25px;
+      .pleft {
+        position: absolute;
+        left:20px;
+      }
+      .pright{
+        position: absolute;
+        left:95px;
+      }
+    }
+  }
+  #ficon {
+    width: 78px;
+    height: 78px;
+    margin: -40px 0 0 0;
+    float: left;
+  }
 }
 </style>
