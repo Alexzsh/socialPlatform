d58497d3177f64464710c4173e08fb09f7e3f544
ckw <tplwang@163.com>
2019-01-09 20:48:53 +0800

    fix friendsNumber momentsNumber

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,52 +3,72 @@ let Person = [
   {
     name: '康天豪',
     headIcon: '1',
-    className: 1801
+    className: 1801,
+    momentsNumber: 3,
+    friendsNumer: 6
   },
   {
     name: '任小健',
     headIcon: '2',
-    className: 1802
+    className: 1802,
+    momentsNumber: 1,
+    friendsNumer: 9
   },
   {
     name: '王鹏程',
     headIcon: '3',
-    className: 1803
+    className: 1803,
+    momentsNumber: 3,
+    friendsNumer: 6
   },
   {
     name: '孙露阳',
     headIcon: '4',
-    className: 1801
+    className: 1801,
+    momentsNumber: 0,
+    friendsNumer: 3
   },
   {
     name: '闫豪',
     headIcon: '5',
-    className: 1803
+    className: 1803,
+    momentsNumber: 2,
+    friendsNumer: 3
   },
   {
     name: '许康琪',
     headIcon: '6',
-    className: 1804
+    className: 1804,
+    momentsNumber: 2,
+    friendsNumer: 3
   },
   {
     name: '施泰龙',
     headIcon: '7',
-    className: 1806
+    className: 1806,
+    momentsNumber: 2,
+    friendsNumer: 5
   },
   {
     name: '黄健雄',
     headIcon: '8',
-    className: 1801
+    className: 1801,
+    momentsNumber: 1,
+    friendsNumer: 2
   },
   {
     name: '肖文君',
     headIcon: '9',
-    className: 1807
+    className: 1807,
+    momentsNumber: 3,
+    friendsNumer: 3
   },
   {
     name: '黄东东',
     headIcon: '10',
-    className: 1801
+    className: 1801,
+    momentsNumber: 2,
+    friendsNumer: 8
   }
 ]
 
@@ -72,5 +92,19 @@ export default {
       })
     })
     return likeList
+  },
+  getMomentsNumber: function (name) {
+    for (let i = 0; i < Person.length; i++) {
+      if (name === Person[i].name) {
+        return Person[i].momentsNumber
+      }
+    }
+  },
+  getFriendsNumber: function (name) {
+    for (let i = 0; i < Person.length; i++) {
+      if (name === Person[i].name) {
+        return Person[i].friendsNumer
+      }
+    }
   }
 }
